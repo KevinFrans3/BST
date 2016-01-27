eb82af52334eebe9e17d6fc2afe348a297bff605
William <awesomewilliamo@gmail.com>
2016-01-27 13:42:42 -0800

    sa
    sa

--- src/BST.java
+++ src/BST.java
@@ -1,45 +1,45 @@
 
 public class BST<T extends Comparable<T>> {
-	
+
 	private T datum;
 	private BST<T> left;
 	private BST<T> right;
-	
-	public BST(T start)
+
+	public BST(T datum)
 	{
-		datum = start;
+
 	}
-	
+
 	public T getDatum()
 	{
 		return datum;
 	}
-	
+
 	public BST<T> getRight()
 	{
 		return right;
 	}
-	
+
 	public BST<T> getLeft()
 	{
 		return left;
 	}
-	
+
 	public void printTree()
 	{
-		
+
 	}
-	
+
 	public String toString()
 	{
-		return "";
+
 	}
-	
+
 	public int depth()
 	{
-		return 0;
+
 	}
-	
+
 	public void insert(T toinsert)
 	{
 		if(toinsert.compareTo(datum) > 0)
