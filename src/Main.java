fc36b3b1f0c55b961a471f41c181a7e7367beaff
William <awesomewilliamo@gmail.com>
2016-01-27 14:09:17 -0800

    f
    f

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,10 @@ public class Main {
 		}
 		
 		t.printTree();
+		
+		//Worst case order of growth for finding a datum is O(n)
+		//Best case order of growth for finding a datum is O(1)
+		//Average order of growth is O(log 2 n), so about 9
 
 	}
 
