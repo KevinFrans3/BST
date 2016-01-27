3fb4cba9048e916fc7184b6c370dbf0efba07e2d
William <awesomewilliamo@gmail.com>
2016-01-27 14:00:40 -0800

    troll
    troll

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-			
-		BST t = new BST(5);
-		t.insert(1);
-		t.insert(2);
-		t.insert(3);
-		t.insert(7);
 
-//		t.printTree();
-		System.out.println(t.depth());
-//		System.out.println(t.getRight().getDatum());
+
+		randp randomlist = new randp(1000);
+
+		BST<Integer> t = new BST<Integer>(randomlist.nextInt());
+		
+		for(int i = 0; i < 999; i++){
+			t.insert(randomlist.nextInt());
+		}
+		
+		t.printTree();
 
 	}
 
