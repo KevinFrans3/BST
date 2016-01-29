1fdb68b592bb4f30f1a334ffcd1aac3ab1cfdc1a
William <awesomewilliamo@gmail.com>
2016-01-29 13:57:21 -0800

    final
    final of legends

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,20 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		randp test = new randp(10);
 
-		for(int i = 0; i < 10; i++)
-		{
-			System.out.println(test.nextInt());
+		randp randomlist = new randp(1000);
+
+		BST<Integer> t = new BST<Integer>(randomlist.nextInt());
+		
+		for(int i = 0; i < 999; i++){
+			t.insert(randomlist.nextInt());
 		}
+		
+		t.printTree();
+		
+		//Worst case order of growth for finding a datum is O(n)
+		//Best case order of growth for finding a datum is O(1)
+		//Average order of growth is O(log 2 n), so about 9
 
 	}
 
